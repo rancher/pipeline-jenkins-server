d69473fff9edc68b56dfbbcaca39c43a1fd7d5e0
gitlawr <lawrleegle@gmail.com>
2019-04-14 21:42:10 +0800

    Use install-plugins script to cache required plugins in the image

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ sed -i 's^${JENKINS_POD_IP}^'"$JENKINS_POD_IP^g" /usr/share/jenkins/ref/config.x
 sed -i 's^${NAMESPACE}^'"$NAMESPACE^g" /usr/share/jenkins/ref/config.xml
 sed -i 's^${SLAVE_IMAGE}^'"$SLAVE_IMAGE^g" /usr/share/jenkins/ref/config.xml
 
-cp -r /usr/share/jenkins/jenkins_home/* /var/jenkins_home
-
 exec /usr/local/bin/jenkins.sh
